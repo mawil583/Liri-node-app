ca82b9c1d7b37a34414ff0d330d6424cbe4873f2
Michael Williams <mwilliams2964@gmail.com>
2019-10-23 16:05:54 -0700

    set up all API calls and dependencies

---
+++ keys.js
@@ -0,0 +1,23 @@
+console.log('this is loaded');
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+// exports.BandsInTown = {
+//   key: process.env.BandsInTownID
+// }
+
+module.exports= {
+  spotify: {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  },
+  BandsInTown: {
+    key: process.env.BandsInTownID
+  },
+  omdb: {
+    key: process.env.omdbID
+  }
+}
