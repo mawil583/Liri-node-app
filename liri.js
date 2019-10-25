eaf4e0100cda987b3d6a84d3bc55f91b9c32429a
Michael Williams <mwilliams2964@gmail.com>
2019-10-25 02:07:22 -0700

    deleted comments and cleaned up

--- liri.js
+++ liri.js
@@ -9,9 +9,11 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 let command = process.argv[2];
 let value = process.argv[3];
+// userInput is used for when appending user entries to log.txt
+// it contains both user arguments
 let userInput = `Command: ${command}; ` +
-    `Value: ${value};
-    `;
+`Value: ${value};
+`;
 
 function switchStatement() {
     switch (command) {
@@ -35,14 +37,14 @@ function switchStatement() {
             appendEntries(userInput);
             console.log(`Invalid Input. For proper functioning, enter 'node liri.js '
                  into the command line and then a command from the switch statment.`);
-    }
+    };
 };
 switchStatement();
 
 function appendEntries(userInput) {
     fs.appendFile("log.txt", userInput, function (err) {
         if (err) throw err;
-    })
+    });
 }
 
 function spotifySearch(song) {
@@ -52,62 +54,36 @@ function spotifySearch(song) {
         };
         if (song == null) {
             console.log("You did not input a song name.");
-            console.log(`Here is an example of the result you would have gotten if you typed "node liri.js spotify-this-song 'the sign'": 
-                artist name: ${data.tracks.items[4].album.artists[0].name} ${data.tracks.items[4].artists[0].name} album name: ${data.tracks.items[4].album.name}`);
-            // spotifyDefaultData();
+            console.log(`Here is an example of the result you would have gotten if you typed "node liri.js spotify-this-song 'the sign'": `);
             console.log("Artist name: " + data.tracks.items[4].artists[0].name);
             console.log("Album name: " + data.tracks.items[4].album.name);
             console.log("Song name: " + data.tracks.items[4].name);
             console.log("Preview URL: " + data.tracks.items[4].preview_url);
         } else {
-            // spotifyData();
             console.log("Artist name: " + data.tracks.items[0].artists[0].name);
             console.log("Album name: " + data.tracks.items[0].album.name);
             console.log("Song name: " + data.tracks.items[0].name);
             console.log("Preview URL: " + data.tracks.items[0].preview_url);
-        }
-        // console.log("Artist name: " + data.tracks.items[0].artists[0].name);
-        // console.log("Album name: " + data.tracks.items[0].album.name);
-        // console.log("Song name: " + data.tracks.items[0].name);
-        // console.log("Preview URL: " + data.tracks.items[0].preview_url);
+        };
     });
 };
 
-// function spotifyData() {
-//     console.log("Artist name: " + data.tracks.items[0].artists[0].name);
-//     console.log("Album name: " + data.tracks.items[0].album.name);
-//     console.log("Song name: " + data.tracks.items[0].name);
-//     console.log("Preview URL: " + data.tracks.items[0].preview_url);
-// }
-
-// function spotifyDefaultData() {
-//     console.log("Artist name: " + data.tracks.items[4].artists[0].name);
-//     console.log("Album name: " + data.tracks.items[4].album.name);
-//     console.log("Song name: " + data.tracks.items[4].name);
-//     console.log("Preview URL: " + data.tracks.items[4].preview_url);
-// }
-
-function bandsSearch(bandName) { // bandname = michael jackson
-    let BandsInTownID = keys.BandsInTown;
+function bandsSearch(bandName) { 
+    let BandsInTownID = keys.BandsInTown.key;
     let bandsQueryUrl = `https://rest.bandsintown.com/artists/${bandName}/events?app_id=${BandsInTownID}`;
     axios.get(bandsQueryUrl).then(
         function (response, err) {
             if (err) throw err;
             for (let i = 0; i < response.data.length; i++) {
-                // name of venue
                 console.log(`Venue Name: ${response.data[i].venue.name}`);
-                // venue location (city, region, country)
                 console.log(`Location: ${response.data[i].venue.city} ` + `${response.data[i].venue.region}, ` + response.data[i].venue.country);
-                // date of event as MM/DD/YYYY
                 console.log(`Event Date: ${moment(response.data[i].datetime).format("MM/DD/YYYY")}`);
             };
-        })
+        });
 };
 
 function omdbSearch(movieName) {
     let omdbID = keys.omdb.key;
-    // let omdbID = keys.omdb; doesn't work
-    console.log(omdbID);
     if (movieName == null) {
         movieName = "Mr. Nobody";
     }
@@ -115,23 +91,15 @@ function omdbSearch(movieName) {
     axios.get(omdbQueryUrl)
         .then(function (response, err) {
             if (err) throw err;
-            // movie title
             console.log("Movie Title: " + response.data.Title);
-            // release year
             console.log("Release Year: " + response.data.Released);
-            // IMDB Rating
             console.log("IMDB rating: " + response.data.Ratings[0].Value);
-            // rotten tomatoes rating
             console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-            // country where movie was produced
             console.log("Country where movie was produced: " + response.data.Country);
-            // language of the movie
             console.log("Language of original movie: " + response.data.Language);
-            // plot
             console.log("Plot: " + response.data.Plot);
-            // actors
             console.log("Actors: " + response.data.Actors);
-        })
+        });
 };
 
 function readRandom() {
@@ -141,9 +109,7 @@ function readRandom() {
         };
         let randomArr = data.split(", ");
         command = randomArr[0];
-        console.log(command)
         value = randomArr[1];
-        console.log(value);
         switchStatement();
     });
 };
